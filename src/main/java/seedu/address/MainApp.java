318f472d97b0f1eaee29c5df8e7fc7ec740ae0b7
E0201942 <aldenlim435@hotmail.com>
2018-11-08 03:05:01 +0800

    checkstyle

--- src/main/java/seedu/address/MainApp.java
+++ src/main/java/seedu/address/MainApp.java
@@ -98,14 +98,13 @@ public class MainApp extends Application {
         } catch (DataConversionException e) {
             logger.warning("Data file not in the correct format. Will be starting with an empty AddressBook");
             initialData = new AddressBook();
-            (
-                    (AddressBook) initialData).setSelf();
+            ((AddressBook) initialData).
+                    setSelf();
         } catch (IOException e) {
             logger.warning("Problem while reading from the file. Will be starting with an empty AddressBook");
             initialData = new AddressBook();
-            (
-                    (AddressBook) initialData).setSelf();
-
+            ((AddressBook) initialData).
+                    setSelf();
         }
 
         return new ModelManager(initialData, userPrefs);
