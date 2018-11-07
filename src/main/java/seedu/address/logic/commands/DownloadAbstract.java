dc03440ea5261c1d9281d3882cc1ed047d946007
leegengyu <gengyu.lee@gmail.com>
2018-11-07 12:38:12 +0800

    Updated DownloadAbstract to restore settings

--- src/main/java/seedu/address/logic/commands/DownloadAbstract.java
+++ src/main/java/seedu/address/logic/commands/DownloadAbstract.java
@@ -64,7 +64,7 @@ public abstract class DownloadAbstract extends Command {
     protected String moduleCode;
     protected String currentDirectoryPath;
     protected String downloadPath;
-    protected boolean isDownloadDisabled = false;
+    protected boolean isDownloadDisabled = true;
 
     public DownloadAbstract(String username, String password, String moduleCode) {
         this.username = username;
