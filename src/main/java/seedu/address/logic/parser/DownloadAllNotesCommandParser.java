bda1c0263901fe0ebc2e20f14be80e6f70141f8c
leegengyu <gengyu.lee@gmail.com>
2018-11-08 13:46:22 +0800

    Update Checkstyle errors

--- src/main/java/seedu/address/logic/parser/DownloadAllNotesCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DownloadAllNotesCommandParser.java
@@ -1,13 +1,16 @@
 package seedu.address.logic.parser;
 //@@author BearPerson1
 
-import seedu.address.logic.commands.DownloadAllNotesCommand;
-import seedu.address.logic.parser.exceptions.ParseException;
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_USERNAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PASSWORD;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_MODULECODE;
 
 import java.util.stream.Stream;
 
-import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.logic.parser.CliSyntax.*;
+import seedu.address.logic.commands.DownloadAllNotesCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
 
 /**
  * DownloadAllNotesCommandParser parses the input that follows after the
