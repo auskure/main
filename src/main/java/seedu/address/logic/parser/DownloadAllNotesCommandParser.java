51aad0aa30ccdd1037d2a1c654916c725a96f723
E0201942 <aldenlim435@hotmail.com>
2018-11-10 07:09:28 +0800

    removed duplicate imports

--- src/main/java/seedu/address/logic/parser/DownloadAllNotesCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DownloadAllNotesCommandParser.java
@@ -7,11 +7,6 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_USERNAME;
 
 import java.util.stream.Stream;
 
-import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_USERNAME;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_PASSWORD;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_MODULECODE;
-
 import seedu.address.logic.commands.DownloadAllNotesCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
