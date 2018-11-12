912cc891b8d28c2571f7d1439c9390bd6caf50f2
E0201942 <aldenlim435@hotmail.com>
2018-11-12 21:12:55 +0800

    removed exception

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -8,16 +8,10 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
-import java.util.List;
-import java.util.Set;
-
-import javafx.collections.ObservableList;
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
-import seedu.address.model.person.IsSelfPredicate;
 import seedu.address.model.person.Person;
-import seedu.address.model.tag.Tag;
 
 /**
  * Adds a person to the address book.
@@ -63,21 +57,10 @@ public class AddCommand extends Command {
     @Override
     public CommandResult execute(Model model, CommandHistory history) throws CommandException {
         requireNonNull(model);
-        List<Person> filteredPersonList = model.getFilteredPersonList();
-        List<Person> selfList = ((ObservableList<Person>) filteredPersonList).filtered(new IsSelfPredicate());
-
         if (model.hasPerson(toAdd)) {
             throw new CommandException(MESSAGE_DUPLICATE_PERSON);
         }
 
-        if (selfList.size() > 0) {
-            Set<Tag> tagCheck = toAdd.getTags();
-            for (Tag check : tagCheck) {
-                if (check.toString().equalsIgnoreCase("[self]")) {
-                    throw new CommandException(MESSAGE_CANNOT_ADD_SELF_TAG);
-                }
-            }
-        }
 
         model.addPerson(toAdd);
         model.commitAddressBook();
