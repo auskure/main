3d7db1c478d491a120419c126f7ba8284cf5b4c0
E0201942 <aldenlim435@hotmail.com>
2018-11-12 20:52:06 +0800

    added new add command exception

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -8,10 +8,16 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
+import java.util.List;
+import java.util.Set;
+
+import javafx.collections.ObservableList;
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
+import seedu.address.model.person.IsSelfPredicate;
 import seedu.address.model.person.Person;
+import seedu.address.model.tag.Tag;
 
 /**
  * Adds a person to the address book.
@@ -41,6 +47,8 @@ public class AddCommand extends Command {
     public static final String MESSAGE_SUCCESS = "New person added: %1$s";
     public static final String MESSAGE_DUPLICATE_PERSON = "This person already exists in the address book";
     public static final String MESSAGE_CANNOT_ADD_MERGED_TAG = "Person cannot be created with a merged tag.";
+    public static final String MESSAGE_CANNOT_ADD_SELF_TAG = "Self contact already exists, this person cannot be "
+            + "created with a self tag.";
 
     private final Person toAdd;
 
@@ -55,9 +63,20 @@ public class AddCommand extends Command {
     @Override
     public CommandResult execute(Model model, CommandHistory history) throws CommandException {
         requireNonNull(model);
+        List<Person> filteredPersonList = model.getFilteredPersonList();
+        List<Person> selfList = ((ObservableList<Person>) filteredPersonList).filtered(new IsSelfPredicate());
 
         if (model.hasPerson(toAdd)) {
             throw new CommandException(MESSAGE_DUPLICATE_PERSON);
+        }
+
+        if (selfList.size() > 0) {
+            Set<Tag> tagCheck = toAdd.getTags();
+            for (Tag check : tagCheck) {
+                if (check.toString().equalsIgnoreCase("[self]")) {
+                    throw new CommandException(MESSAGE_CANNOT_ADD_SELF_TAG);
+                }
+            }
         }
 
         model.addPerson(toAdd);
